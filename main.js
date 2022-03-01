6997ed110a04a8daab0db9b2c8a0d9162e0ca22e
frants-jeon <jjh6963@naver.com>
2022-03-01 19:28:26 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const str = `
+010-1234-5678
+jjh6963@naver.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
